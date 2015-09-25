afd50e3b331c997a2bec18921b60b6ebccfd33d4
Patrick Stein <lpstein@yahoo-inc.com>
2015-09-25 14:33:47 -0700

    Wireframing

---
+++ Sneezeboard/Sneezeboard/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  Sneezeboard
+//
+//  Created by Patrick Stein on 9/25/15.
+//  Copyright © 2015 patrick. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+  @IBAction func backTouched(sender: AnyObject) {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+}
---
+++ Sneezeboard/Sneezeboard/SignupController.swift
@@ -0,0 +1,13 @@
+//
+//  SignupController.swift
+//  Sneezeboard
+//
+//  Created by Patrick Stein on 9/25/15.
+//  Copyright © 2015 patrick. All rights reserved.
+//
+
+import UIKit
+
+class SignupController: UIViewController {
+
+}
