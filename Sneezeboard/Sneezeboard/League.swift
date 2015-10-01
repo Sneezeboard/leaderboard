5af4aa2a6e46d9af293ae1d10d5228674f04f179
willdalton@gmail.com <wdalton@tumblr.com>
2015-09-30 17:13:04 -0700

    sp

---
+++ Sneezeboard/Sneezeboard/League.swift
@@ -0,0 +1,13 @@
+//
+//  League.swift
+//  Sneezeboard
+//
+//  Created by Will Dalton on 9/30/15.
+//  Copyright © 2015 patrick. All rights reserved.
+//
+
+import UIKit
+
+class League: NSObject {
+
+}
