dd1a49871c5923169806af7ec7edc6dab68646ab
Patrick Stein <lpstein@yahoo-inc.com>
2015-10-07 09:35:56 -0700

    Add some pods

--- Sneezeboard/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Sneezeboard/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -54,20 +54,24 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods/AFNetworking.framework'
   install_framework 'Pods/Bolts.framework'
+  install_framework 'Pods/ESTabBarController.framework'
   install_framework 'Pods/FBSDKCoreKit.framework'
   install_framework 'Pods/FBSDKLoginKit.framework'
   install_framework 'Pods/FBSDKShareKit.framework'
   install_framework 'Pods/Parse.framework'
   install_framework 'Pods/ParseFacebookUtilsV4.framework'
   install_framework 'Pods/ParseUI.framework'
+  install_framework 'Pods/RESideMenu.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods/AFNetworking.framework'
   install_framework 'Pods/Bolts.framework'
+  install_framework 'Pods/ESTabBarController.framework'
   install_framework 'Pods/FBSDKCoreKit.framework'
   install_framework 'Pods/FBSDKLoginKit.framework'
   install_framework 'Pods/FBSDKShareKit.framework'
   install_framework 'Pods/Parse.framework'
   install_framework 'Pods/ParseFacebookUtilsV4.framework'
   install_framework 'Pods/ParseUI.framework'
+  install_framework 'Pods/RESideMenu.framework'
 fi
